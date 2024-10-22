79a1a503e55b2ae0fff03f36946c193d716a4b37
s2213353 <changwenjie1999@gmail.com>
2024-10-22 13:14:13 +0200

    add buy/sell equipment func

--- NinjaManager.Data/obj/Debug/net8.0/NinjaManager.Data.AssemblyInfo.cs
+++ NinjaManager.Data/obj/Debug/net8.0/NinjaManager.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NinjaManager.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16177af8b20c93006683346dfd8716a8ead6fde9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5abee9f3ceec1378cd83ca0d911efdef2a6b953")]
 [assembly: System.Reflection.AssemblyProductAttribute("NinjaManager.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NinjaManager.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- NinjaManager/Controllers/InventoryController.cs
+++ NinjaManager/Controllers/InventoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NinjaManager.Data;
+using NinjaManager.Models;
 
 namespace NinjaManager.Controllers
 {
@@ -19,10 +20,94 @@ namespace NinjaManager.Controllers
                 .Include(n => n.Inventory)
                 .ThenInclude(i => i.Equipment)
                 .FirstOrDefault(n => n.NinjaId == ninjaId);
-
             if (ninjaEquipment == null) return NotFound();
 
             return View("Index", ninjaEquipment);
+        }
+
+        [HttpGet]
+        public IActionResult Buy(int ninjaId)
+        {
+            var ninja = _context.Ninjas
+                .Include(n => n.Inventory)
+                .FirstOrDefault(n => n.NinjaId == ninjaId);
+            if (ninja == null) return NotFound();
+
+            var ninjaEquipmentIds = ninja.Inventory.Select(i => i.EquipmentId).ToList();
+            var availableEquipment = _context.Equipment
+                .Where(e => !ninjaEquipmentIds
+                .Contains(e.EquipmentId))
+                .ToList();
+
+            ViewBag.NinjaId = ninjaId;
+
+            return View(availableEquipment);
+        }
+
+        [HttpPost]
+        public IActionResult Buy(int ninjaId, int equipmentId)
+        {
+            var ninja = _context.Ninjas
+                .Include(n => n.Inventory)
+                .ThenInclude(i => i.Equipment)
+                .FirstOrDefault(n => n.NinjaId == ninjaId);
+            if (ninja == null) return NotFound();
+
+
+            var equipment = _context.Equipment.Find(equipmentId);
+            if (equipment == null) return NotFound();
+
+            var existingCategoryEquipment = ninja.Inventory.Any(i => i.Equipment.Category == equipment.Category);
+            if (existingCategoryEquipment)
+            {
+                TempData["ErrorMessage"] = $"You already have equipment in the {equipment.Category} category!";
+
+                return RedirectToAction("Buy", new { ninjaId = ninjaId });
+            }
+
+            if (ninja.Gold >= equipment.GoldValue)
+            {
+                ninja.Gold -= equipment.GoldValue;
+
+                _context.Inventories.Add(new InventoryModel
+                {
+                    NinjaId = ninjaId,
+                    EquipmentId = equipmentId,
+                });
+                _context.SaveChanges();
+
+                TempData["SuccessMessage"] = $"Successfully bought {equipment.Name}!";
+
+            } else {
+                TempData["ErrorMessage"] = $"You lack {equipment.GoldValue - ninja.Gold} gold to purchase this equipment!";
+            }
+
+            return RedirectToAction("Buy", new { ninjaId });
+        }
+
+        [HttpPost]
+        public IActionResult Sell(int ninjaId, int equipmentId)
+        {
+            var ninja = _context.Ninjas
+                .Include(n => n.Inventory)
+                .ThenInclude(i => i.Equipment)
+                .FirstOrDefault(n => n.NinjaId == ninjaId);
+            if (ninja == null) return NotFound();
+
+            var inventoryItem = ninja.Inventory.FirstOrDefault(i => i.EquipmentId == equipmentId);
+            if (inventoryItem == null) return NotFound();
+
+            var equipment = inventoryItem.Equipment;
+            if (equipment == null) return NotFound();
+
+            ninja.Gold += equipment.GoldValue;
+
+            _context.Inventories.Remove(inventoryItem);
+            _context.SaveChanges();
+
+            TempData["SuccessMessage"] = $"Successfully sold {equipment.Name}!";
+
+            return RedirectToAction("Index", new { ninjaId });
         }
     }
 }
